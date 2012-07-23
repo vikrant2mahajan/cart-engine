8918665efb02991cf730b592111f26cfe051eda5
vikrant2mahajan@gmail.com <vikrant2mahajan@gmail.com@84eba1f9-b223-a926-e6fe-3b3e116b6070>
2012-07-23 10:29:27 +0000

    Distance Finder

--- cart-engine/src/main/java/com/mmt/services/utils/DistanceFinder.java
+++ cart-engine/src/main/java/com/mmt/services/utils/DistanceFinder.java
@@ -15,6 +15,18 @@ public class DistanceFinder {
 	
 	private String toCity;
 
+	
+	public DistanceFinder() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DistanceFinder(String fromCity, String toCity) {
+		super();
+		this.fromCity = fromCity;
+		this.toCity = toCity;
+	}
+
 	public HttplClient getClient() {
 		return client;
 	}
@@ -39,11 +51,27 @@ public class DistanceFinder {
 		this.toCity = toCity;
 	}
 
-	public void getGeoLocation(){
+	public String getDistance(){
 		String url = "http://www.world-airport-codes.com/dist/?a1="+fromCity+"&a2="+toCity;
 		client.setUrl(url);
 		client.executeMethod();
 		String response = client.getResponse();
+		return parse(response);
+	}
+
+	private String parse(String res){
+		String str[] = res.split("The distance has been calculated as being:");
+		String str5 = null;
+		if(str.length>1){
+			String str1[] =str[1].split("kilometres");
+			String str2 = str1[0].trim();
+			String str3 = str2.replace("<b>", "");
+			String str4 = str3.replace("</b>", "");
+			str5 = str4.trim();
+		} else {
+			System.out.println("ERROR::: ");	
+		}
+		return str5;
 	}
 
 }
